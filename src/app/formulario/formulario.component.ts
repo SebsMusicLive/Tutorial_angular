46b1447fdc53c9761bb98ec04a66e5dead7ad5d7
SebsMusicLive <lopezortegajohansebastian@gmail.com>
2024-09-07 14:54:35 -0500

    clase 10

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+
+  openAlert:boolean = false;
+  
   mostrar_en_consola(name:string){
     console.log(name);
+
+    this.openAlert = true;
+
   }
 }
