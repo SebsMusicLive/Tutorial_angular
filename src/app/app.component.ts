95a87eace6bdaba1af605adf4000b8dc3946e308
SebsMusicLive <lopezortegajohansebastian@gmail.com>
2024-09-08 22:57:19 -0500

    clase 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { ContadorComponent } from "./contador/contador.component";
 import { BotonesComponent } from "./botones/botones.component";
 import { FormularioComponent } from "./formulario/formulario.component";
 import { BuclesComponent } from './bucles/bucles.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContadorComponent, BotonesComponent, FormularioComponent, BuclesComponent],
+  imports: [RouterOutlet, ContadorComponent, BotonesComponent, FormularioComponent, BuclesComponent, SwitchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
